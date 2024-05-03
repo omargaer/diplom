490cd14f931ef0b323422258fc0730695ad24b29
omargaer <omarololoshkin@gmail.com>
2024-05-03 16:03:35 +1000

    сделал разметку главной страницы

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 #маршрутизация проекта
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name = 'index'),
 ]
